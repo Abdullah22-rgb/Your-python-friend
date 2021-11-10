8d2c7dd8dfab484ed08300e51b75ca44df454541
Abdullah22-rgb <69909984+Abdullah22-rgb@users.noreply.github.com>
2021-11-10 18:31:33 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+name = input("Hello what is your name? ")
+input("Hello " + name)
+input("I'm Max nice to meat you! ")
+age = input("How old are you? ")
+input ("I'm a 11 too! ")
+input("Hey " + name)
+Thing = input("What do you want to play? ")
+input("Lets play " + Thing)
+input("We had fun! ")
+thing = input("Bye " + name)
+
+
+for i in range(5):
+    print ( "this is numer ")
